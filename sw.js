c313e94e3b6ffae97eb236c3376369832672af96
maikongsp <maikongsp@gmail.com>
2026-09-20 00:40:17 -0300

    style(footer): exibe somente o icone do LinkedIn sem o texto

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * registre fotos e emita laudos mesmo sem conexão à internet.
  */
 
-const CACHE_NAME = 'locar-inspecao-v2.4';
+const CACHE_NAME = 'locar-inspecao-v2.5';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
